8d530232cc737d57749cbda58630e8ac0fcab245
Jesse <jdnely@gmail.com>
2021-08-20 16:40:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, message) {
+    let cardMessage = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cardMessage.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+   return cardMessage;
+}
+console.log (writeCards);
+
+
+function countDown(){
+    let i = 10
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+
+
+}
